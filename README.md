a260753c099aaf473a26fd509928e52fffccb135
Son Dam Ngoc <Damngocson@outlook.com.vn>
2021-01-21 15:01:18 +0700

    Feature Serilog with ConsoleApp .NetCore 3.1

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Serilog_ConsoleApp3.1
 This project demo log file and log console with console app .NetCore 3.1
 
+Note:
+File log default path: {Project}\bin\Debug
 
-## File log default path: {Project}\bin\Debug
+Show log UI with Serilog Seq
+B1: Download software Seq serilog https://datalust.co/download 
+	or docker Seq Home: https://hub.docker.com/r/datalust/seq
+	link docker: docker run --name seq -d --restart unless-stopped -e ACCEPT_EULA=Y -p 5341:80 datalust/seq:latest
+
+B2: Add .WriteTo.Seq("http://localhost:5341") and Log.CloseAndFlush() in code
+B3: Addresst default http://localhost:5341/  show log.
---
+++ SerilogConsoleApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+using System;
+
+namespace SerilogConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyLogger.CreateLogger();
+            Log.ForContext("SourceContext", "Main").Information("Start");
+            TestLoger logerTest = new TestLoger();
+
+
+            Log.CloseAndFlush();
+            Console.ReadKey();
+        }
+    }
+
+    public static class MyLogger
+    {
+        public static void CreateLogger()
+        {
+            string logTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u}] [{SourceContext}] {Message}{NewLine}{Exception}";
+            var dateTime = DateTime.Now;
+            string dateString = dateTime.ToString("dd-MM-yyyy");
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console(theme: AnsiConsoleTheme.Code, outputTemplate: logTemplate)
+                .WriteTo.File($"log-{dateString}.txt", outputTemplate: logTemplate)
+                // .WriteTo.LiterateConsole()
+                .WriteTo.Seq("http://localhost:5341")
+                .CreateLogger();
+        }
+    }
+
+    class TestLoger
+    {
+        public TestLoger()
+        {
+            Log.ForContext("SourceContext", typeof(TestLoger).Name).Information("Wirte log Information level 1");
+            // Verbose and Debug hidden
+            Log.ForContext("SourceContext", typeof(TestLoger).Name).Verbose("wirte log Verbose level 2");
+            Log.ForContext("SourceContext", typeof(TestLoger).Name).Debug("wirte log Debug level 3");
+
+            Log.ForContext("SourceContext", typeof(TestLoger).Name).Warning("wirte log Warning level 4");
+            Log.ForContext("SourceContext", typeof(TestLoger).Name).Error("wirte log Error level 5");
+            Log.ForContext("SourceContext", typeof(TestLoger).Name).Fatal("wirte log Fatal level 6");
+
+        }
+    }
+}
